33bfed87d7a7c009e4ab0f97b567ca7f4a6aaa3d
Santiago Rueda <Santiago.Rueda@fravega.com.ar>
2023-08-28 21:33:16 -0300

    mockeo funcion

--- .py
+++
@@ -1,4 +0,0 @@
-def prueba(num1, num2):
-    return num1 + num2
-
-
--- tests/test_funciones.py
+++ tests/test_funciones.py
@@ -6,5 +6,37 @@ from servicio.funciones import suma
 # En visual studio code configuro la carpeta donde están lost test.
 # Carpeta que se llame test y las funciones arranquen con test.
 
+from unittest.mock import Mock
+
+# Supongo que es aleatorio
+class Suerte:
+
+    def __init__(self) -> None:
+        pass
+    def tirar_dado():
+        return 5
+
+
 def test_funcion_sumar():
-    assert suma(2,3) == 5
+    # Mockeo una clase
+    mock = Mock(spec=Suerte)
+    mock.tirar_dado.return_value = 7
+    assert suma(2,mock.tirar_dado()) == 9
+
+
+# MOCKEO DE FUNCION
+
+# from unittest.mock import patch
+
+# # Mockeamos la función usando el decorador patch
+# @patch('__main__.funcion_original')
+# def test_funcion_mockeada(mock_funcion_original):
+#     # Configuramos el comportamiento simulado
+#     mock_funcion_original.return_value = "Función mockeada"
+
+#     # Ahora puedes utilizar mock_funcion_original en lugar de funcion_original en tus pruebas
+#     resultado = funcion_original()
+#     print(resultado)  # Imprimirá "Función mockeada"
+
+# # Ejecutamos la prueba
+# test_funcion_mockeada()
